2cf45a959e6c50139b16cc9253f6b57f904547fa
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-08-22 08:46:11 -0300

    Projeto Inicializado

---
+++ contact-apit/src/main/java/com/talissonmelo/contactapit/ContactApitApplication.java
@@ -0,0 +1,13 @@
+package com.talissonmelo.contactapit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactApitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactApitApplication.class, args);
+	}
+
+}
---
+++ contact-apit/src/test/java/com/talissonmelo/contactapit/ContactApitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talissonmelo.contactapit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactApitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
