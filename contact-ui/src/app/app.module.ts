415bd5f6feae42816757c261f9734aefda3df107
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-08-26 19:18:09 -0300

    Utilizando o Angular Material

--- contact-ui/src/app/app.module.ts
+++ contact-ui/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button'
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
