e2251725b7f5904ce892fcfaac37ef4096ce107e
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-08-28 18:13:14 -0300

    Formulário de contatos

--- contact-ui/src/app/app.module.ts
+++ contact-ui/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatInputModule } from '@angular/material/input'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,6 +18,8 @@ import { ContactService } from './contact.service'
   imports: [
     BrowserModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
     
     BrowserAnimationsModule
   ],
