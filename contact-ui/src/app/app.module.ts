49c117c04607f122f75794975c397926fc02378f
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-08-29 11:15:42 -0300

    Melhorias no layout

--- contact-ui/src/app/app.module.ts
+++ contact-ui/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { MatButtonModule } from '@angular/material/button'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatInputModule } from '@angular/material/input'
 import { ReactiveFormsModule } from '@angular/forms'
+import {MatTabsModule} from '@angular/material/tabs'
+import {MatTableModule } from '@angular/material/table'
+import {MatIconModule} from '@angular/material/icon'
+import {MatCardModule} from '@angular/material/card'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -24,7 +28,11 @@ import { ContactService } from './contact.service'
     MatInputModule,
     HttpClientModule, 
     ReactiveFormsModule,  
-    
+    MatTabsModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
+
     BrowserAnimationsModule
   ],
   providers: [ContactService],
